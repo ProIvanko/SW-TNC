9ae95920431bc02709c63719e8dda1a7e1a73a93
Ivan_Lovkov <91388543+ProIvanko@users.noreply.github.com>
2023-05-31 16:43:06 +0300

    Delete Project directory

--- Project/src/loginWindow.js
+++
@@ -1,6 +0,0 @@
-const loginWindow = {
-    innerHTML : ''
-    
-}
-
-export {loginWindow}
--- Project/src/three_libs_import.js
+++
@@ -1,7 +0,0 @@
-import * as THREE from 'https://cdn.skypack.dev/three@0.136/build/three.module.js'
-import {GLTFLoader} from 'https://cdn.skypack.dev/three@0.136/examples/jsm/loaders/GLTFLoader.js';
-import {OrbitControls} from 'https://cdn.skypack.dev/three@0.136/examples/jsm/controls/OrbitControls.js';
-import { FlyControls } from 'https://cdn.skypack.dev/three@0.136/examples/jsm/controls/FlyControls.js';
-
-
-export {THREE, GLTFLoader, OrbitControls, FlyControls}
